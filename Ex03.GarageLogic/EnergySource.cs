cbf32c60521b71ee4ac6b72289e8c2cc704985d7
mayageva2 <mayageva7@gmail.com>
2026-05-26 11:16:18 +0300

    adding motorcycle types + energySource

---
+++ Ex03.GarageLogic/EnergySource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal abstract class EnergySource
+    {
+        protected float m_CurrentEnergyAmount;
+        protected float m_MaxEnergyAmount;
+
+        protected EnergySource(float i_MaxEnergyAmount)
+        {
+            m_MaxEnergyAmount = i_MaxEnergyAmount;
+            m_CurrentEnergyAmount = 0;
+        }
+
+        protected void AddEnergy (float i_EnergyAmountToAdd)
+        {
+            if(i_EnergyAmountToAdd < 0)
+            {
+                //exception
+            }
+            
+            if(m_CurrentEnergyAmount + i_EnergyAmountToAdd > m_MaxEnergyAmount)
+            {
+                //exception
+            }
+
+            m_CurrentEnergyAmount += i_EnergyAmountToAdd;
+        }
+    }
+}
