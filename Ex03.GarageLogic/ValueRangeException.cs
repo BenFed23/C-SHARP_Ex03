f14c9f8936eb3cd4576cee12549d6ab93e626704
mayageva2 <mayageva7@gmail.com>
2026-05-28 22:05:41 +0300

    creating ValueRangeException, and adding exceptions instead of notes

---
+++ Ex03.GarageLogic/ValueRangeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    public class ValueRangeException : Exception
+    {
+        public float m_MaxValue {  get; set; }
+        public float m_MinValue { get; set; }
+
+        public ValueRangeException(float i_MinValue, float i_MaxValue, string i_Message = null)
+        {
+            m_MaxValue = i_MaxValue;
+            m_MinValue = i_MinValue;
+        }
+    }
+}
