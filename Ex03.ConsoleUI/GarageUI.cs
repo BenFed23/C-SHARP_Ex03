147d6b52b26567beab4b34003a8cb03b76d4b6d0
Ben Fediuck <benfe@mta.ac.il>
2026-05-30 15:35:26 +0300

    GarageUI

---
+++ Ex03.ConsoleUI/GarageUI.cs
@@ -0,0 +1,45 @@
+﻿
+using Ex03.GarageLogic;
+using System;
+using System.Collections.Generic;
+
+
+namespace Ex03.ConsoleUI
+{
+    internal class GarageUI
+    {
+        GarageManager m_garageManager;
+        public GarageUI(GarageManager i_garageManager)
+        {
+            m_garageManager = i_garageManager;
+        }
+        public void  ShowPlateList()
+        {
+            Dictionary<string, GarageVehicle> garageDictionary = m_garageManager.GetDictionary();
+            foreach(string plate in garageDictionary.Keys) 
+            {
+                Console.WriteLine(plate);
+            }
+        }
+        public void ShowPlateLisAcourdingToState(eGarageStatus i_carStatus) 
+        {
+            List<string> carPlatesWithGivenState = new List<string>();
+            Dictionary<string, GarageVehicle> garageDictionary = m_garageManager.GetDictionary();
+            foreach (KeyValuePair< string, GarageVehicle > car  in garageDictionary)
+            {
+                if(car.Value.GarageStatus == i_carStatus)
+                {
+                    carPlatesWithGivenState.Add(car.Key);
+                }
+            }
+            PrintGivenList(carPlatesWithGivenState);
+        }
+        public void PrintGivenList(List<string> i_givenList) 
+        {
+            foreach (string value in i_givenList)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
--- Ex03.GarageLogic/GarageVehicle.cs
+++ Ex03.GarageLogic/GarageVehicle.cs
@@ -16,5 +16,17 @@ namespace Ex03.GarageLogic
             m_OwnerPhone = i_OwnerPhone;
             m_VehicleStatus = eGarageStatus.InRepair;
         }
+        public eGarageStatus GarageStatus
+        {
+            get
+            {
+                return m_VehicleStatus; 
+            }
+            set
+            {
+                m_VehicleStatus = value;
+            }
+        }
+        
     }
 }
